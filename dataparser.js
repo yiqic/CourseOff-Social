332705995d692949e9b54a564da590204cccf445
Yiqi (Victor) Chen <yiqic1993@gatech.edu>
2015-04-17 22:11:18 -0400

    fix parse data

--- dataparser.js
+++ dataparser.js
@@ -31,31 +31,32 @@ var parseSchedule = function(schedule) {
 	var newSchedule = {};
 	$.each(schedule, function(i1, friend) {
 		$.each(friend["info"], function(i2, term) {
-			if (!(term in newSchedule)) {
-				newSchedule[term] = [];
+			var term_ident = term["term_ident"];
+			if (!(term_ident in newSchedule)) {
+				newSchedule[term_ident] = [];
 			}
 			$.each(term["courses"], function(i3, course) {
 				var exist = false;
 				var index;
-				$.each(newSchedule[term], function(i, d) {
+				$.each(newSchedule[term_ident], function(i, d) {
 					if (d["major_ident"] == course["major_ident"] && d["course_ident"] == course["course_ident"]) {
 						exist = true;
 						index = i;
 					}
 				})
 				if (!exist) {
-					newSchedule[term].push({
+					newSchedule[term_ident].push({
 						"major_ident": course["major_ident"], 
 						"course_ident": course["course_ident"], 
 						"sections": {}
 					});
-					index = newSchedule[term].length - 1;
+					index = newSchedule[term_ident].length - 1;
 				}
 				$.each(course["sections"], function(i4, section) {
-					if (!(section in newSchedule[term][index])) {
-						newSchedule[term][index]["sections"][section] = []
+					if (!(section in newSchedule[term_ident][index])) {
+						newSchedule[term_ident][index]["sections"][section] = []
 					}
-					newSchedule[term][index]["sections"][section].push(friend["id"]);
+					newSchedule[term_ident][index]["sections"][section].push(friend["id"]);
 				});
 			});
 		});
