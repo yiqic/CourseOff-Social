788b7829a4cb20b24f524ce0d5ba6eb85a755867
Yiqi (Victor) Chen <yiqic1993@gatech.edu>
2015-04-18 11:17:42 -0400

    make sync load

--- dataparser.js
+++ dataparser.js
@@ -2,24 +2,20 @@
 var getFriendList = function(callback) {
 	$.ajax({
 	  url: "https://gatech.courseoff.com/api/me/friends",
+	  async: false
 	})
 	.done(function(data) {
 		callback(data);
-	})
-	.error(function() {
-		alert("unable to get friend list");
 	});
 };
 
 var getFriendScheduleById = function(id, callback) {
 	$.ajax({
 	  url: "https://gatech.courseoff.com/api/users/" + id + "/schedules",
+	  async: false
 	})
 	.done(function(data) {
 		callback(id, data);
-	})
-	.error(function() {
-		alert("unable to get schedule of a friend");
 	});
 };
 
