3757a8a6e98b8b0897755f519c8f351b3b0d14e4
Haoli Du <henry54809@gmail.com>
2015-04-17 20:58:46 -0400

    init commit for friend service

---
+++ friendservice.js
@@ -0,0 +1,62 @@
+var userService = {
+	friendsList : [],
+	rawSchedules: [],
+	schedules: {},
+	ready: false,
+	init : function(){
+		getFriendList(this.consumeFriendsList);
+	},
+	getFriendsList : function(){
+		return this.friendsList;
+	},
+	getFriendScheduleById : function(id){
+		if(!this.ready){
+			console.log('Not ready when getFriendScheduleById');
+			return null;
+		}
+		for(var i=0; i < rawSchedules.length; i++){
+			var friendId = rawSchedules[i].id;
+			if(friendId === id){
+				return rawSchedules[i].info;
+			}
+		}
+		return null;
+	},
+	getFriendsByCourseId : function(id){
+		if(!this.ready){
+			console.log('Not ready when getFriendScheduleById');
+			return null;
+		}
+
+	},
+	consumeFriendsList : function(friendsList){
+		this.friendsList = friendsList;
+		for(var i=0; i< friendsList.length; i++){
+			getFriendScheduleById( friendsList[i].id, this.consumeSchedule );
+		}
+	},
+	consumeSchedule: function(schedule){
+		if(!schedule.owner_id){
+			console.log('Structure for schedule is not as expected: ');
+			console.log(schedule);
+			return;
+		}
+		for(var i=0; i < friendsList[i].length; i++){
+			var friendId = friendsList[i].id;
+			if( schedule.owner_id === friendId ){
+				console.log('Consuming schedule for ' + friendsList[i].name);
+				var processed = {};
+				processed.id = friendId;
+				processed.name = friendsList[i].name;
+				processed.info = schedule;
+				rawSchedules.push(processed);
+			}
+		}
+		if( rawSchedules.length === friendsList.length ){
+			this.schedules = parseSchedule(this.rawSchedules);
+			this.ready = true;
+		}
+	}
+};
+
+//userService.init();
